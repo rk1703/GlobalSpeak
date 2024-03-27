f24edb07badcf14982a92f05685f1677b4ec8882
Ravikant <rk1703world@gmail.com>
2024-03-27 23:20:25 +0530

    adding languages

--- components/TranslationLanguages.tsx
+++ components/TranslationLanguages.tsx
@@ -1,11 +1,80 @@
 "use client"
 
 import { TranslationLanguages } from "@/app/translate/page";
+import { Textarea } from "@/components/ui/textarea"
 
-function TranslationForm({languages}:{languages:TranslationLanguages}){
-    return(
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+function TranslationForm({ languages }: { languages: TranslationLanguages }) {
+    return (
         <div>
-            <form></form>
+            <form>
+                <div>
+                    <Select name="inputLanguages" defaultValue="auto">
+                        <SelectTrigger className="w-[280px]">
+                            <SelectValue placeholder="Select a Language" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>Want us to figure it out?</SelectLabel>
+                                <SelectItem key="auto" value="auto">Auto-Detection</SelectItem>
+
+                            </SelectGroup>
+                            <SelectGroup>
+                                <SelectLabel>
+                                    Languages
+                                </SelectLabel>
+                                {Object.entries(languages.translation).map(([key, value]) => (
+                                    <SelectItem key={key} value={key}>{value.name}</SelectItem>
+                                ))}
+                            </SelectGroup>
+
+                        </SelectContent>
+                    </Select>
+                    <Textarea
+                        placeholder="Type your message here"
+                        name="input"
+                        className="min-h-32 text-xl"
+                    />
+                </div>
+                <div>
+                    <Select>
+                        <SelectTrigger className="w-[280px]">
+                            <SelectValue placeholder="Select a Language" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>Want us to figure it out?</SelectLabel>
+                                <SelectItem key="auto" value="auto">Auto-Detection</SelectItem>
+
+                            </SelectGroup>
+                            <SelectGroup>
+                                <SelectLabel>
+                                    Languages
+                                </SelectLabel>
+                                {Object.entries(languages.translation).map(([key, value]) => (
+                                    <SelectItem key={key} value={key}>{value.name}</SelectItem>
+                                ))}
+                            </SelectGroup>
+
+                        </SelectContent>
+                    </Select>
+                    <Textarea
+                        placeholder="Type your message here"
+                        name="input"
+                        className="min-h-32 text-xl"
+                    />
+                </div>
+
+            </form>
         </div>
     )
 }
